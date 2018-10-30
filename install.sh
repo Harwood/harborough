e89296ee67450783a2709a4259efbf36b9f3998e
Harwood <1222752+Harwood@users.noreply.github.com>
2018-10-29 22:42:11 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,6 +8,14 @@
 ## Install Homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+## Fish Shell
+brew install fish
+
+## Krypton
+brew install kryptco/tap/kr
+kr pair
+kr codesign
+
 ## Install Keybase
 brew cask install keybase
 keybase login harwood
@@ -16,4 +24,9 @@ keybase login harwood
 brew install yadm
 
 ## yadm clone --bootstrap <repo_url>
-yadm clone --bootstrap keybase://private/harwood/harborough
+yadm clone --bootstrap --recurse-submodules keybase://private/harwood/harborough
+
+## Install vim
+brew install jq vim
+curl -sLf https://spacevim.org/install.sh | bash -s -- --install vim
+git -C ~/.SpaceVim checkout $(curl -s https://api.github.com/repos/SpaceVim/SpaceVim/releases | jq 'max_by(.tag_name)' | jq -r '.tag_name')
